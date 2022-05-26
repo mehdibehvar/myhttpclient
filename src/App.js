95bf877c206ed432061c4d47be05a9fb6254b4de
mehdibehvar <mehdibehvar00@gmail.com>
2022-05-26 22:10:06 +0430

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
 import './App.css';
+import { del, get, patch, post, put } from './services/httpClient';
+
 
 function App() {
+  useEffect(()=>{
+    del("posts/2");
+
+    get("posts",{params:{id:1}})
+    .then(response=>console.log(response));
+
+    post('users', {
+        title: 'foopost',
+        body: 'bar',
+        userId: 1
+    }).then(response=>console.log(response));
+
+   put("posts/1",{
+    id: 1,
+    title: 'fooput',
+    userId: 1,
+  }).then(response=>console.log(response));
+
+
+
+  patch("posts/1",{
+    title: 'foopatch',
+  }).then(response=>console.log(response))
+
+  
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <h1>axios service</h1>
     </div>
+    </>
   );
 }
 
